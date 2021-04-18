5772a1e55a6fa63d2198ccfa4ce5bb7267abec10
Max <lucas97lopes@hotmail.com>
2021-04-18 17:16:58 -0300

    melhoria: adição de sons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,46 @@
 import { ThemeContext } from 'styled-components'
 import { useContext } from 'react'
 import * as S from '../../styles/components/Header/styles'
-import Image from 'next/image'
 import Link from 'next/link'
-import useSound from 'use-sound';
 interface Props {
     toggleTheme: () => void;
 }
 
 function Header(props: Props) {
     const { colors, title } = useContext(ThemeContext)
-    
+
+    function handleClick() {
+        props.toggleTheme()
+
+        if(title === "dark") {
+            return new Audio('/sounds/switch-on.mp3').play()
+        } else {
+            return new Audio('/sounds/switch-off.mp3').play()
+        }
+    }
+
+    function mouseOnMenu() {
+        return new Audio('/sounds/menu-open.mp3').play()
+    }
+
     return (
         <div className="container">
             <S.Container>
                 <S.Logo>
-                    <Link href="/">
-                        Sr.Max  
-                    </Link>
+                    <a href="/" onMouseOver={mouseOnMenu}>
+                        Sr.Max
+                    </a>
                 </S.Logo>
                 <S.Nav>
                     <S.Button
-                        onClick={props.toggleTheme}
+                        onClick={handleClick}
                     >
                         <S.Alert className="material-icons material-icons-outlined">expand_more</S.Alert>
-                        <img src="/images/sol.svg" alt="sol" style={title === 'dark' ? {width: '40px'} : {width: '0px'}} />
-                        <img src="/images/lua.svg" alt="lua" style={title === 'light' ? {width: '40px'} : {width: '0px'}} />
+                        <img src="/images/sol.svg" alt="sol" style={title === 'dark' ? { width: '40px' } : { width: '0px' }} />
+                        <img src="/images/lua.svg" alt="lua" style={title === 'light' ? { width: '40px' } : { width: '0px' }} />
                     </S.Button>
-                    <Link href="/">Inicio</Link>
-                    <Link href="/about">Sobre</Link>
+                    <a href="/" onMouseOver={mouseOnMenu}>Inicio</a>
+                    <a href="/about" onMouseOver={mouseOnMenu}>Sobre</a>
                 </S.Nav>
 
             </S.Container>
