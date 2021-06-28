8f190a33097d903b18168b3338575be7a30dbb69
max <lucas97lopes@hotmail.com>
2021-06-27 23:06:24 -0300

    hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,6 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet"></link>
                     <script dangerouslySetInnerHTML={{
                         __html: `<!-- Hotjar Tracking Code for https://lucasmaximiano-dev.vercel.app/ -->
-                        <script>
                             (function(h,o,t,j,a,r){
                                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                                 h._hjSettings={hjid:2475337,hjsv:6};
@@ -46,8 +45,7 @@ export default class MyDocument extends Document {
                                 r=o.createElement('script');r.async=1;
                                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                                 a.appendChild(r);
-                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                        </script>`}} />
+                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
                 </Head>
                 <body>
                     <Main />
