567520b296a53770553468d73b1a5845ba47ef8e
max <lucas97lopes@hotmail.com>
2021-06-27 22:57:12 -0300

    hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components'
 
+
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const sheet = new ServerStyleSheet()
@@ -35,6 +36,18 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;400;900&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet"></link>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `<!-- Hotjar Tracking Code for https://lucasmaximiano-dev.vercel.app/ -->
+                        <script>
+                            (function(h,o,t,j,a,r){
+                                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                                h._hjSettings={hjid:2475337,hjsv:6};
+                                a=o.getElementsByTagName('head')[0];
+                                r=o.createElement('script');r.async=1;
+                                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                                a.appendChild(r);
+                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                        </script>`}} />
                 </Head>
                 <body>
                     <Main />
